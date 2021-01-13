64931262e2fe48a738487f0da3335346c0fa37f0
SpookyJelly <74237436+SpookyJelly@users.noreply.github.com>
2021-01-13 15:42:44 +0900

    Add files via upload

---
+++ TIL/day01/hello2.py
@@ -0,0 +1,9 @@
+greeting = 'hello, world!'
+
+count = 0
+while count < 4:
+    print(greeting)
+    count+=1
+
+for i in range(5):
+    print(greeting)
---
+++ TIL/day01/lunch.py
@@ -0,0 +1,3 @@
+lunch =['초밥','광주 사업장 웰스토리','서브웨이','짜장면']
+print(lunch)
+print(lunch[2])
